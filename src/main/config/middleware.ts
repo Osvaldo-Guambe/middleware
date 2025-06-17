b76d5a3718d84d601797a24ccf425aa079ce4848
pluscodelda <osvaldofilipeguambe@gmail.com>
2025-06-17 20:42:00 +0200

    feat:  add uuid4

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,10 +1,15 @@
 import { type Express } from "express";
 import { bodyParser, helmetApp, morganApp } from "../middlewares";
 import { corsApp } from "../middlewares/cors";
+import { v4 as uuidv4 } from "uuid";
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(corsApp);
   app.use(helmetApp);
   app.use(morganApp);
+  app.use((req, res, next) => {
+    res.setHeader("X-Request-ID", uuidv4());
+    next();
+  });
 };
